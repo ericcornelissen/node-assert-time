0fafefcf948709730e4ed4b5d465e786d62ab43b
Eric Cornelissen <ericornelissen@gmail.com>
2026-06-03 00:03:23 +0200

    Add TypeScript type definitions for `assertTime`

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+type Asyncfut = () => Promise<unknown>;
+type SyncFut = () => unknown;
+
+type Callback = (duration: number) => void;
+
+export function assertTime(fut: SyncFut, deadline: number): number;
+export function assertTime(fut: Asyncfut, deadline: number): Promise<number>;
+export function assertTime(fut: SyncFut, deadline: number, onSlow: Callback, onTime: Callback): void;
