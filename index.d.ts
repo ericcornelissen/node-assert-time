4b553027c4f610f870b6a9c040191db688d75bc9
Eric Cornelissen <ericornelissen@gmail.com>
2026-06-03 22:47:43 +0200

    Proper types for async API

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,8 @@ type SyncFut = () => unknown;
 
 type Callback = (duration: number) => void;
 
-export function assertTime(fut: SyncFut, deadline: number): number;
 export function assertTime(fut: Asyncfut, deadline: number): Promise<number>;
-export function assertTime(fut: SyncFut, deadline: number, onSlow: Callback): void;
-export function assertTime(fut: SyncFut, deadline: number, onSlow: Callback, onTime: Callback): void;
+export function assertTime(fut: Asyncfut, deadline: number, onSlow: Callback, onTime: Callback?): void;
+
+export function assertTime(fut: SyncFut, deadline: number): number;
+export function assertTime(fut: SyncFut, deadline: number, onSlow: Callback, onTime: Callback?): void;
