2078d5d5b81b003eac6dda68ca22613f574adcfe
Eric Cornelissen <ericornelissen@gmail.com>
2026-06-03 23:19:10 +0200

    consistency

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-type Asyncfut = () => Promise<unknown>;
+type AsyncFut = () => Promise<unknown>;
 type SyncFut = () => unknown;
 
 type Callback = (duration: number) => void;
 
-export function assertTime(fut: Asyncfut, deadline: number): Promise<number>;
-export function assertTime(fut: Asyncfut, deadline: number, onSlow: Callback, onTime: Callback?): void;
+export function assertTime(fut: AsyncFut, deadline: number): Promise<number>;
+export function assertTime(fut: AsyncFut, deadline: number, onSlow: Callback, onTime: Callback?): void;
 
 export function assertTime(fut: SyncFut, deadline: number): number;
 export function assertTime(fut: SyncFut, deadline: number, onSlow: Callback, onTime: Callback?): void;
